9972f9f8eea1604f238a9a614c08162f610e854c
Tony <makotra.anton@prachka.space>
2023-12-11 22:01:43 +0300

    50% task1

--- src/main/java/fire/guard/analog/fireguard/Task1Controller.java
+++ src/main/java/fire/guard/analog/fireguard/Task1Controller.java
@@ -7,18 +7,40 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.util.Map;
 import java.util.ResourceBundle;
+
 
 public class Task1Controller implements Initializable {
     @FXML
     private Label welcomeText;
     @FXML
-    private ChoiceBox<String> choiceBox;
+    private ChoiceBox<String> dropDownList;
+    @FXML
+    private TextField pressureField;
+    @FXML
+    private TextField volumeField;
 
-    private final String[] items = {"Ацетилен", "Бутан","Бутен","Метан","Пропилен","Этан","Этилен"};
+    private final Map<String,Integer> gasMap = Map.of("Ацетилен", 26,"Бутан", 58,"Бутен", 56,"Метан", 16,"Пропилен", 42,"Этан", 30,"Этилен", 28);
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        for(var item : gasMap.keySet()) {
+            dropDownList.getItems().add(item);
+        }
+    }
+
+    public void onTest(){
+        try {
+            System.out.println(pressureField.getText());
+            System.out.println(volumeField.getText());
+            System.out.println(gasMap.get(dropDownList.getValue()));
+        }
+        catch(Exception e){
+            e.getCause();
+        }
 
     }
+
+
 }
