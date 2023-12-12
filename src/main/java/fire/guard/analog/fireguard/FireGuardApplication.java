7db424f7702e209b91d5430540934489e2b7a19f
Tony <makotra.anton@prachka.space>
2023-12-12 21:59:13 +0300

    main.fxml, task1.fxml, task2.fxml structure added, 2 controllers available. !!!WARNING!!! task2.fxml is empty, please use simple Pane, not TabPane

--- src/main/java/fire/guard/analog/fireguard/FireGuardApplication.java
+++ src/main/java/fire/guard/analog/fireguard/FireGuardApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class FireGuardApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(FireGuardApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(FireGuardApplication.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 900);
         stage.setTitle("Горючие газы - Отчет");
         stage.setScene(scene);
