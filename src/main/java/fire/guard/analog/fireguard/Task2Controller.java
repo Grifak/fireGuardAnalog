defdb234cb1306f8ee68dcd30227b885f188e843
Grifak <86059775+Grifak@users.noreply.github.com>
2023-12-13 20:53:36 +0300

    ui final version

--- src/main/java/fire/guard/analog/fireguard/Task2Controller.java
+++ src/main/java/fire/guard/analog/fireguard/Task2Controller.java
@@ -10,26 +10,30 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 
 public class Task2Controller implements Initializable {
-
     @FXML
     private ChoiceBox<String> listTask2;
-    @FXML
-    private ChoiceBox<Double> airSpeedList;
-    @FXML
-    private ChoiceBox<Integer> tempList;
-
     @FXML
     private TextField molarMassField;
     @FXML
     private TextField densityField;
+
+
+    //Пункт 6
     @FXML
     private TextField formulaField;
+    @FXML
+    private ChoiceBox<Integer> tempList;
+    @FXML
+    private TextField antuanA;
+    @FXML
+    private TextField antuanB;
+    @FXML
+    private TextField antuanC;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         listTask2.getItems().addAll(Task2Substance.getNames());
         listTask2.setOnAction(this::getSubstanceValue);
-
-        airSpeedList.getItems().addAll(0.0, 0.1, 0.2, 0.5, 1.0);
 
         tempList.getItems().addAll(10, 15, 20, 30, 35);
     }
@@ -38,5 +42,8 @@ public class Task2Controller implements Initializable {
         Task2Substance substance = Task2Substance.getByName(listTask2.getValue());
         molarMassField.setText(substance.getMolarMass().toString());
         formulaField.setText(substance.getFormula());
+        antuanA.setText(substance.getAntuanA().toString());
+        antuanB.setText(substance.getAntuanB().toString());
+        antuanC.setText(substance.getAntuanC().toString());
     }
 }
--- src/main/java/fire/guard/analog/fireguard/enums/Task2Substance.java
+++ src/main/java/fire/guard/analog/fireguard/enums/Task2Substance.java
@@ -4,21 +4,28 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum Task2Substance {
-    GEKSAN("гексан", "C6 H10 O", 98),
-    CHIKLO_GEKSAN("циклогексан", "С6 H14", 86),
-    BUTILACHETAT("бутилацетат", "C2 H12 O2", 116),
-    ACETON("ацетон", "C3 H6 O", 58),
-    PENTAN("пентан", "С5 H12", 72),
-    SPIRT("этиловый спирт", "С2 H6 O", 46),
-    BENZOL("бензол", "C6 H6", 78);
+    GEKSAN("гексан", "C6 H10 O", 98, 6.33089, 1670.009, 230.312),
+    CHIKLO_GEKSAN("циклогексан", "С6 H14", 86, 5.99517, 1166.274, 223.661),
+    BUTILACHETAT("бутилацетат", "C2 H12 O2", 116, 6.25205, 1430.418, 210.745),
+    ACETON("ацетон", "C3 H6 O", 58, 6.37551, 1281.721, 237.008),
+    PENTAN("пентан", "С5 H12", 72, 5.97208, 1062.555, 231.805),
+    SPIRT("этиловый спирт", "С2 H6 O", 46, 7.81158, 1918.508, 252.125),
+    BENZOL("бензол", "C6 H6", 78, 5.6139, 902.275, 178.099);
     private String name;
     private String formula;
     private Integer molarMass;
+    private Double antuanA;
+    private Double antuanB;
+    private Double antuanC;
 
-    Task2Substance(String name, String formula, Integer molarMass) {
+
+    Task2Substance(String name, String formula, Integer molarMass, Double antuanA, Double antuanB, Double antuanC) {
         this.name = name;
         this.formula = formula;
         this.molarMass = molarMass;
+        this.antuanA = antuanA;
+        this.antuanB = antuanB;
+        this.antuanC = antuanC;
     }
 
     public String getName() {
@@ -32,7 +39,17 @@ public enum Task2Substance {
     public Integer getMolarMass() {
         return molarMass;
     }
+    public Double getAntuanA() {
+        return antuanA;
+    }
 
+    public Double getAntuanB() {
+        return antuanB;
+    }
+
+    public Double getAntuanC() {
+        return antuanC;
+    }
     public static List<String> getNames(){
         return Arrays.stream(Task2Substance.values())
                 .map(Task2Substance::getName)
