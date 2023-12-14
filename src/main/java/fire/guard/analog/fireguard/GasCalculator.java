1019ce6b643971437443837cf27a4046b1a1220c
Tony <makotra.anton@prachka.space>
2023-12-15 00:39:00 +0300

    calculations ready for test

--- src/main/java/fire/guard/analog/fireguard/GasCalculator.java
+++ src/main/java/fire/guard/analog/fireguard/GasCalculator.java
@@ -1,51 +1,51 @@
-package fire.guard.analog.fireguard.GasCalculation;
+package fire.guard.analog.fireguard;
 
 public class GasCalculator {
     private static final double V0 = 22.4;
     private static final double PI = Math.PI;
 
     //1 Method to calculate Va
-    public static double calculateVa(double P, double V) {
+    public static Double calculateVa(Double P, Double V) {
         return 0.01 * P * V;
     }
 
     //2 Method to calculate V1m
-    public static double calculateV1m(double q, double tau) {
+    public static Double calculateV1m(Double q, Double tau) {
         return q * tau;
     }
 
     //3 Method to calculate V2m
-    public static double calculateV2m(double P2, double lPodv, double dPodv, double lOtv, double dOtv) {
+    public static Double calculateV2m(Double P2, Double lPodv, Double dPodv, Double lOtv, double dOtv) {
         return 0.01 * PI * P2 * (((lPodv * Math.pow(dPodv, 2))/4) + ((lOtv * Math.pow(dOtv, 2))/4));
     }
 
     //4 Method to calculate ρg
-    public static double calculateRhoG(double M, double T) {
+    public static Double calculateRhoG(double M, double T) {
         return M / (V0 * (1 + 0.00367 * T));
     }
 
     //5 Method to calculate m
-    public static double calculateM(double Va, double Vm, double rho) {
+    public static Double calculateM(Double Va, Double Vm, Double rho) {
         return (Va + Vm) * rho;
     }
 
     //6 Method to calculate m*
-    public static double calculateMStar(double m, double A, double tau) {
+    public static Double calculateMStar(Double m, Double A, Double tau) {
         return m / (1 + A * tau);
     }
     //8 Method to calculate Vсв
-    public static double calculateVsv(double L, double B, double H) {
+    public static Double calculateVsv(Double L, Double B, Double H, Double areaCoef) {
         double Vpom = L * B * H;
-        return 0.8 * Vpom;
+        return areaCoef * Vpom;
     }
 
     //9 Method to calculate Сстех
-    public static double calculateCsteh(double nC, double nH, double nX, double nO) {
+    public static Double calculateCsteh(Double nC, Double nH, Double nX, Double nO) {
         double beta = nC + ((nH - nX) / 4) - (nO / 2);
         return 100 / (1 + (4.84 * beta));
     }
     //11 Method to calculate ΔP
-    public static double calculateDeltaP(double Pmax, double P0, double m, double Z, double Vsv, double rhoG, double Csteh, double Kn) {
+    public static Double calculateDeltaP(Double Pmax, Double P0, Double m, Double Z, Double Vsv, Double rhoG, Double Csteh, Double Kn) {
         return (Pmax - P0) * ((m * Z) / (Vsv * rhoG)) * (100 / Csteh) * (1 / Kn);
     }
 }
--- src/main/java/fire/guard/analog/fireguard/Task1Controller.java
+++ src/main/java/fire/guard/analog/fireguard/Task1Controller.java
@@ -1,5 +1,7 @@
 package fire.guard.analog.fireguard;
 
+import fire.guard.analog.fireguard.enums.Task1Stehio;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
@@ -12,7 +14,7 @@ import java.util.ResourceBundle;
 
 
 public class Task1Controller implements Initializable {
-
+    private final ValuesStorage valuesStorage = new ValuesStorage();
     @FXML
     private ChoiceBox<String> dropDownList;
     @FXML
@@ -40,7 +42,7 @@ public class Task1Controller implements Initializable {
     @FXML
     private TextField outGasField;
     @FXML
-    private TextField insideTempField1;
+    private TextField inGasField;
     @FXML
     private TextField airExchgField;
     @FXML
@@ -50,9 +52,13 @@ public class Task1Controller implements Initializable {
     @FXML
     private TextField widthField;
     @FXML
-    private TextField lengthField1;
+    private TextField areaCoefField;
+    private Double NcField;
+    private Double NhField;
     @FXML
-    private TextField widthField1;
+    private TextField NxField;
+    @FXML
+    private TextField NoField;
     @FXML
     private TextField maxPressureField;
     @FXML
@@ -62,29 +68,98 @@ public class Task1Controller implements Initializable {
     @FXML
     private ChoiceBox<String> dropDownListSteh;
     @FXML
+    private TextField res1;
+    @FXML
+    private TextField res2;
+    @FXML
+    private TextField res3;
+    @FXML
+    private TextField res4;
+    @FXML
+    private TextField res5;
+    @FXML
+    private TextField res6;
+    @FXML
+    private TextField res7;
+    @FXML
+    private TextField res8;
+    @FXML
+    private TextField res9;
+    @FXML
+    private TextField res10;
+    @FXML
     private Label warningLabel;
     private final Map<String,Integer> gasMap = Map.of("Ацетилен", 26,"Бутан", 58,"Бутен", 56,"Метан", 16,"Пропилен", 42,"Этан", 30,"Этилен", 28);
 
-    private final Float[] coefficientZ = {1.0F, 0.5F, 0.3F, 0.0F};
+    private final Double[] coefficientZ = {1.0, 0.5, 0.3, 0.0};
+
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        for (var item : gasMap.keySet()) {
+        for(var item : gasMap.keySet()) {
             dropDownList.getItems().add(item);
         }
         for(var item : coefficientZ){
             dropDownListZ.getItems().add(String.valueOf(item));
         }
+        dropDownListSteh.getItems().addAll(Task1Stehio.getNames());
+        dropDownListSteh.setOnAction(this::getStehioValue);
+
     }
 
-    public void onGetValues(){
+    public void getStehioValue(ActionEvent actionEvent){
+        Task1Stehio stehio = Task1Stehio.getByName(dropDownListSteh.getValue());
+        NcField = Double.valueOf(stehio.getNc());
+        NhField = Double.valueOf(stehio.getNh());
+
+    }
+
+    public void fillResultingValues(){
+        res1.setText(String.valueOf(gasMap.get(dropDownList.getValue())));
+        res2.setText(String.valueOf(valuesStorage.getVa()));
+        res3.setText(String.valueOf(valuesStorage.getV1m()));
+        res4.setText(String.valueOf(valuesStorage.getV2m()));
+        res5.setText(String.valueOf(valuesStorage.getRhoG()));
+        res6.setText(String.valueOf(valuesStorage.getM()));
+        res7.setText(String.valueOf(valuesStorage.getMstar()));
+        res8.setText(String.valueOf(valuesStorage.getVsv()));
+        res9.setText(String.valueOf(valuesStorage.getCsteh()));
+        res10.setText(String.valueOf(valuesStorage.getDeltaP()));
+    }
+
+    public void onGetValues(ActionEvent actionEvent){
         try {
-            System.out.println(pressureField.getText());
-            System.out.println(volumeField.getText());
-            System.out.println(gasMap.get(dropDownList.getValue()));
+            valuesStorage.setVa(GasCalculator.calculateVa(Double.parseDouble(pressureField.getText()), Double.parseDouble(volumeField.getText())));
+            valuesStorage.setV1m(GasCalculator.calculateV1m(Double.parseDouble(gasConsumptionField.getText()), Double.parseDouble(closingTimeField.getText())));
+            valuesStorage.setV2m(GasCalculator.calculateV2m(Double.parseDouble(maxP2Field.getText()), Double.parseDouble(lpodvField.getText()),
+                    Double.parseDouble(dodvField.getText()), Double.parseDouble(lotvField.getText()),
+                    Double.parseDouble(dotvField.getText())));
+
+            valuesStorage.setRhoG(GasCalculator.calculateRhoG(gasMap.get(dropDownList.getValue()), Double.parseDouble(insideTempField.getText())));
+            valuesStorage.setM(GasCalculator.calculateM(Double.parseDouble(inGasField.getText()), Double.parseDouble(outGasField.getText()), valuesStorage.getRhoG()));
+            valuesStorage.setMstar(GasCalculator.calculateMStar(valuesStorage.getM(), Double.parseDouble(airExchgField.getText()), Double.parseDouble(closingTimeField.getText())));
+            valuesStorage.setCoefZ(Double.parseDouble(dropDownListZ.getValue()));
+            valuesStorage.setVsv(GasCalculator.calculateVsv(Double.parseDouble(lengthField.getText()), Double.parseDouble(widthField.getText()),
+                    Double.parseDouble(heightField.getText()), Double.parseDouble(areaCoefField.getText())));
+
+            valuesStorage.setCsteh(GasCalculator.calculateCsteh(NcField, NhField, Double.parseDouble(NxField.getText()), Double.parseDouble(NoField.getText())));
+
+            valuesStorage.setDeltaP(GasCalculator.calculateDeltaP(Double.parseDouble(maxPressureField.getText()),
+                    Double.parseDouble(startPressureField.getText()), valuesStorage.getMstar(),
+                    valuesStorage.getCoefZ(), valuesStorage.getVsv(), valuesStorage.getRhoG(),
+                    valuesStorage.getCsteh(), Double.valueOf(valuesStorage.getKn())));
+
+            fillResultingValues();
         }
-        catch(Exception e){
-            e.getCause();
+        catch (ArithmeticException arithmeticException){
+            arithmeticException.getCause();
+            warningLabel.setText("Ошибка в веденных значениях! Попробуйте еще раз");
         }
 
+    }
+
+
+    public void onGenerateReport(ActionEvent actionEvent) {
     }
 }
