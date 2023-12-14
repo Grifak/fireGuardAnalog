7d3d9bc6c1cf258cc6720618c9c517e514fe06bd
Tony <makotra.anton@prachka.space>
2023-12-15 00:39:00 +0300

    calculations ready for test

---
+++ src/main/java/fire/guard/analog/fireguard/ValuesStorage.java
@@ -0,0 +1,107 @@
+package fire.guard.analog.fireguard;
+
+
+public class ValuesStorage {
+
+    private Double Va;
+    private Double V1m;
+    private Double V2m;
+    private Double RhoG;
+    private Double M;
+    private Double Mstar;
+    private Double Vsv;
+
+    public Integer getKn() {
+        return Kn;
+    }
+
+    public void setKn(Integer kn) {
+        Kn = kn;
+    }
+
+    private Integer Kn = 3;
+    private Double coefZ;
+
+    private Double Csteh;
+    private Double DeltaP;
+
+    public Double getCoefZ() {
+        return coefZ;
+    }
+
+    public void setCoefZ(Double coefZ) {
+        this.coefZ = coefZ;
+    }
+
+    public Double getVa() {
+        return Va;
+    }
+
+    public void setVa(Double va) {
+        Va = va;
+    }
+
+    public Double getV1m() {
+        return V1m;
+    }
+
+    public void setV1m(Double v1m) {
+        V1m = v1m;
+    }
+
+    public Double getV2m() {
+        return V2m;
+    }
+
+    public void setV2m(Double v2m) {
+        V2m = v2m;
+    }
+
+    public Double getRhoG() {
+        return RhoG;
+    }
+
+    public void setRhoG(Double rhoG) {
+        RhoG = rhoG;
+    }
+
+    public Double getM() {
+        return M;
+    }
+
+    public void setM(Double m) {
+        M = m;
+    }
+
+    public Double getMstar() {
+        return Mstar;
+    }
+
+    public void setMstar(Double mstar) {
+        Mstar = mstar;
+    }
+
+    public Double getVsv() {
+        return Vsv;
+    }
+
+    public void setVsv(Double vsv) {
+        Vsv = vsv;
+    }
+
+    public Double getCsteh() {
+        return Csteh;
+    }
+
+    public void setCsteh(Double csteh) {
+        Csteh = csteh;
+    }
+
+    public Double getDeltaP() {
+        return DeltaP;
+    }
+
+    public void setDeltaP(Double deltaP) {
+        DeltaP = deltaP;
+    }
+}
--- src/main/java/fire/guard/analog/fireguard/enums/Task1Stehio.java
+++ src/main/java/fire/guard/analog/fireguard/enums/Task1Stehio.java
@@ -1,15 +1,33 @@
 package fire.guard.analog.fireguard.enums;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum Task1Stehio {
-    ACETYLENE(2,2),
-    BUTANE(4,10),
-    BUTENE(4,8),
-    METHANE(1,4),
-    PROPYLENE(3,6),
-    ETHAN(2,6),
-    ETHYLENE(2,4);
+    ACETYLENE("Ацетилен",2,2),
+    BUTANE("Бутан",4,10),
+    BUTENE("Бутен",4,8),
+    METHANE("Метан",1,4),
+    PROPYLENE("Пропилен",3,6),
+    ETHAN("Этан",2,6),
+    ETHYLENE("Этилен",2,4);
+
+    private String name;
     private Integer Nc;
     private Integer Nh;
+
+    public static List<String> getNames(){
+        return Arrays.stream(Task1Stehio.values())
+                .map(Task1Stehio::getName)
+                .toList();
+    }
+
+    public static Task1Stehio getByName(String name){
+        return Arrays.stream(Task1Stehio.values())
+                .filter(val -> val.getName().equals(name))
+                .findFirst()
+                .orElse(null);
+    }
 
     public Integer getNc() {
         return Nc;
@@ -17,6 +35,14 @@ public enum Task1Stehio {
 
     public void setNc(Integer nc) {
         Nc = nc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public Integer getNh() {
@@ -27,7 +53,8 @@ public enum Task1Stehio {
         Nh = nh;
     }
 
-    Task1Stehio(Integer Nc, Integer Nh){
+    Task1Stehio(String name, Integer Nc, Integer Nh){
+        this.name = name;
         this.Nc = Nc;
         this.Nh = Nh;
     }
